15acfb330235efcfd01afe6078ce8c69d88295e0
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2024-08-09 19:49:25 +0200

    feat: current home page as about page

--- components/layouts/About.tsx
+++ components/layouts/About.tsx
@@ -1,22 +1,17 @@
-import Image from "next/image"
 import Header from "../shared/Header"
 
 const About = () => {
   return (
-    <section className="grid grid-cols-2 h-screen bg-neutral-300" id="about">
-      <div className="flex flex-col justify-center w-[40rem] h-screen mx-auto px-10 bg-orange-700">
-        <Header text={'ŁUKASZ ŚLIWIŃSKI'} />
-        <article className="text-lg my-6 text-justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </article>
+    <section className="bg-gray-950" id="about">
+      <div className="grid grid-cols-2 h-screen bg-local bg-fit-h bg-no-repeat bg-left bg-landing">
+        <div className="col-start-2 flex flex-col items-end w-[40rem] m-auto p-10 bg-neutral-300/30">
+          <Header text={'ŁUKASZ ŚLIWIŃSKI'} />
+          <article className="text-lg my-6 text-right">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </article>
+        </div>
       </div>
-      <Image
-        src="/profile.jpg"
-        className="m-auto"
-        width={450}
-        height={600}
-        alt="About me pictures"
-      />
     </section>
   )
 }
