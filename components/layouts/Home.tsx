3d40027c702ea3b3b374b7da0bc42c5b41894c6a
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2024-08-11 21:10:31 +0200

    feat: update home section

--- components/layouts/Home.tsx
+++ components/layouts/Home.tsx
@@ -1,6 +1,33 @@
+import Image from "next/image";
+import logo from "../../public/logo.svg";
+import Button from "../shared/Button";
+
 const Home = () => {
   return (
-    <section className="grid grid-cols-2 h-screen bg-gray-950" id="home">
+    <section className="relative bg-cover bg-center bg-no-repeat bg-landing" id="home">
+      
+      <div className="absolute w-full">
+        <Image className="drop-shadow-lg opacity-5 mx-auto mt-12" src={logo} width="0" height="250" alt="logo" />
+
+      </div>
+      
+      
+      <div className="flex flex-col justify-center items-center h-screen bg-black/70">
+
+        
+        <h1 className="font-light text-neutral-300 my-24">
+          <span className="block text-4xl tracking-[1.02rem] mb-4">ŁUKASZ ŚLIWIŃSKI</span>
+          <span className="block text-7xl tracking-widest">Z GÓRĄ W TLE</span>
+        </h1>
+        <div className="grid grid-cols-3 gap-8">
+          <Button label="O MNIE" href="#about" />
+          <Button label="MUZYKA" href="#music" />
+          <Button label="LINKI" href="#links" />
+        </div>
+      
+      </div>
+      <div className="absolute bottom-0 w-1/2 h-1/5 bg-black left-cover" ></div>
+      <div className="absolute bottom-0 right-0 w-1/2 h-1/5 bg-black right-cover" ></div>
     </section>
   )
 }
---
+++ components/shared/Button.tsx
@@ -0,0 +1,18 @@
+interface Props {
+  label: string,
+  href: string
+}
+
+const Button = ({ label, href }: Props) => {
+  return (
+    <a
+      type="button"
+      href={href}
+      className="inline-block rounded border-2 border-neutral-300 w-32 py-2 text-center font-medium uppercase leading-normal text-neutral-300 transition duration-150 ease-in-out hover:bg-neutral-300 hover:text-neutral-700 focus:bg-neutral-300 focus:text-neutral-700 focus:outline-none focus:ring-0 active:bg-neutral-300 active:text-neutral-700 motion-reduce:transition-none"
+    >
+      {label}
+    </a>
+  )
+}
+
+export default Button
