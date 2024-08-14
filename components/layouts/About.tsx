83d384372f5aa90c73723c38c276e8578843de49
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2024-08-14 11:30:20 +0200

    fix: correct profile photo

--- components/layouts/About.tsx
+++ components/layouts/About.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import profile from '../../public/profile.png';
+import profile from '../../public/profile2.png';
 import Paragraph from '../shared/Paragraph';
 
 const About = () => {
