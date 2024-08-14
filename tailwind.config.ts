b23fa7eba9648ab23e0e71488bd68b512b81b46d
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2024-08-14 11:44:42 +0200

    fix: correct colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,7 +36,7 @@ const config: Config = {
         'screen-mobile': 'calc(var(--vh, 1vh) * 100)'
       },
       colors: {
-        'main-text': '#e5e5e5',
+        'main-text': '#fafafa',
         'dark-text': '#404040',
         'dark-bg': '#09090b',
         'light-bg': '#e5e5e5'
