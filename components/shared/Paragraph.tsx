c24e23a5b3c78f95fc4c7e486e3ee1b9b370e323
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2024-12-01 14:12:15 +0100

    fix: correct font

--- components/shared/Paragraph.tsx
+++ components/shared/Paragraph.tsx
@@ -1,8 +1,8 @@
 const Paragraph = ({ header, text }: { header: string; text: string }) => {
   return (
-    <div className="m-auto w-4/5 max-w-160">
+    <div className="mx-6 sm:m-auto sm:w-4/5 max-w-160">
       <h2 className="text-xl xs:text-3xl xl:text-4xl">{header}</h2>
-      <p className="text-sm xs:text-base xl:text-lg mt-6">{text}</p>
+      <p className="text-xs xs:text-sm sm:text-base xl:text-lg mt-6">{text}</p>
     </div>
   );
 };
